f3e0e8d45e0734ca0af49ca4e20f7e755f877017
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2023-11-06 19:30:32 +0100

    Uppgift2

---
+++ src/main/java/org/Uppgifter/Uppgift2.java
@@ -0,0 +1,18 @@
+package org.Uppgifter;
+
+public class Uppgift2 {
+
+    public static void main(String[] args) {
+
+        int a = 11;
+
+        if (a%2==0){
+            System.out.println("Talet var jämnt");
+        }else {
+            System.out.println("Talet var udda");
+        }
+
+
+    }
+
+}
