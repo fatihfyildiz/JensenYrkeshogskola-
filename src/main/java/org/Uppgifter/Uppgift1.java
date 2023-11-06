243bdadeb83df4f2da86f0936f8210d6989632b3
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2023-11-06 19:12:47 +0100

    uppgift1 last

--- src/main/java/org/Uppgifter/Uppgift1.java
+++ src/main/java/org/Uppgifter/Uppgift1.java
@@ -1,8 +1,7 @@
 package org.Uppgifter;
 
 public class Uppgift1 {
-
-    public class Uppgift1 {
+    
         public Uppgift1() {
         }
 
@@ -10,8 +9,9 @@ public class Uppgift1 {
             int a = 10;
             int b = 5;
             int c = Math.max(a, b);
+
             System.out.println(c);
         }
-    }
+
 
 }
