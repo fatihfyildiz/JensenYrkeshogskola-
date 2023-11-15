6b9a8435218ddd5fb438d0c80d87c421849ddf14
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2023-11-15 10:44:32 +0100

    Chat Bot Test

---
+++ src/main/java/org/example/JUnit/Lektion10_15_11_23/ChatBotTest.java
@@ -0,0 +1,19 @@
+package org.example.JUnit.Lektion10_15_11_23;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChatBotTest {
+
+    @Test
+    public void testReply(){
+        ChatBot chatBot= new ChatBot();
+        String reply= chatBot.reply("Hello!");
+
+        assertEquals("Hi!", reply);
+    }
+
+}
