07bfb933d97220122c20578c5b396d7f132d3b21
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2023-11-20 10:35:15 +0100

    BufferedWriter - Write to a File

---
+++ src/main/java/org/example/JUnit/Lektion10_20_11_23/WriteToFile.java
@@ -0,0 +1,28 @@
+package org.example.JUnit.Lektion10_20_11_23;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+
+    public static void main(String[] args) {
+
+        try {
+            FileWriter writer= new FileWriter("example.txt");
+            BufferedWriter bufferedWriter= new BufferedWriter(writer);
+
+            bufferedWriter.write("Hello World!");
+            bufferedWriter.newLine();
+            bufferedWriter.write("Welcome to Java file handling.");
+
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
+
+}
