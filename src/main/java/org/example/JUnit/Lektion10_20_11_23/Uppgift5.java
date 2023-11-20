ec5c24300e6a81c402542c82b0c669f83f63857b
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2023-11-20 13:21:41 +0100

    Uppgift7

---
+++ src/main/java/org/example/JUnit/Lektion10_20_11_23/Uppgift5.java
@@ -0,0 +1,31 @@
+package org.example.JUnit.Lektion10_20_11_23;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Uppgift5 {
+    public static void main(String[] args) {
+        System.out.println(get_lines("practice.txt"));
+
+    }
+
+    public static int get_lines (String path) {
+
+        int lines = 0;
+
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while (bufferedReader.readLine() != null) {
+                lines++;
+            }
+
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
