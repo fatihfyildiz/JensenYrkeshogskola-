5be1d30c8e6c896643881748d8f6123d1b5513c1
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2023-11-28 10:50:00 +0100

    API testing with PathParam

--- src/main/java/org/example/JUnit_Lektioner9_10/Lektion11_27_11_23/UserApiIntegrationTest.java
+++ src/main/java/org/example/JUnit_Lektioner9_10/Lektion11_27_11_23/UserApiIntegrationTest.java
@@ -16,13 +16,37 @@ public class UserApiIntegrationTest {
                 .body("name", equalTo("Leanne Graham"))
                 .body("email", equalTo("Sincere@april.biz"));
     }
+
     @Test
     public void testApiFetchUser_TWO() {
         given().baseUri("https://jsonplaceholder.typicode.com")
                 .when().get("/users/2")
                 .then().assertThat().statusCode(200)
-                .body("name", equalTo("INCORRECT"))
-                .body("email", equalTo("INCORRECT"));
+                .body("name", equalTo("Ervin Howell"))
+                .body("email", equalTo("Shanna@melissa.tv"));
+    }
+
+    @Test
+    public void testApiÖvning1() {
+        given().baseUri("https://jsonplaceholder.typicode.com")
+                .when().get("/posts/1")
+                .then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void testApiÖvning2() {
+        given().baseUri("https://jsonplaceholder.typicode.com")
+                .when().get("/posts/1")
+                .then().assertThat().statusCode(200)
+                .body("title", equalTo("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+    }
+
+    @Test
+    public void testApiÖvning3() {
+        given().baseUri("https://jsonplaceholder.typicode.com")
+                .pathParam("userId",1)
+                .when().get("/users/{userId}")
+                .then().assertThat().statusCode(200);
     }
 
 }
